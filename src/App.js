0cfbb900705c37786bacd8fc69cbe4af0b723956
JhuneAlfred <avelicaria@me.com>
2017-10-03 00:04:51 -0700

    new

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import C from './constants'
+import {allSkiDays, goal} from './initialState.json'
+
+export default () => {  
+    return (
+      <div className="App">
+       <Header/>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  
+}
+
+//export default App;
+
+
+const Header = () => (
+ <div>
+ Ski Day Counter <br/>
+ =============== <br/>
+ The goal is ${goal} days<br/>
+ Initially there are ${allSkiDays.length} ski days in state<br/><br/>
+
+ Constants (actions)<br/>
+ --------------------<br/>
+ ${Object.keys(C).join('\n      ')} 
+</div>
+)
