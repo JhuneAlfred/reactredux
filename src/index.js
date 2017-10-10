551ac12b0cbe0ac2769885300e510057d811ae5e
JhuneAlfred <avelicaria@me.com>
2017-10-09 23:40:15 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import C from './constants'
+import { goal } from './store/reducers'
+
+const state = 10
+
+const action = {
+    type: C.SET_GOAL,
+    payload: 15
+}
+
+const nextState = goal(state, action)
+
+console.log(`
+    initial goal: ${state}
+    action: ${JSON.stringify(action)}
+    new goal: ${nextState}
+`)
